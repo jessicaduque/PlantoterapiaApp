76611e7ee008d23c0fd91e932d709f73953175e7
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2021-12-17 19:37:14 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/plantoterapiaapp/ConhecaProjetoActivity.java
@@ -0,0 +1,14 @@
+package com.example.plantoterapiaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConhecaProjetoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conheca_projeto);
+    }
+}
